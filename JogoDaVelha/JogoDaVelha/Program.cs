67484c58877ce0b8bf8263fc13894c3fa4406b86
Marco Ribeiro <marcosousacontato@gmail.com>
2022-12-21 12:59:30 -0300

    Começando projeto jogo da velha, com funções MostrarVelha e Jogar

---
+++ JogoDaVelha/JogoDaVelha/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace JogoDaVelha
+{
+    class Program
+    {
+        static string[,] mat = new string[3, 3];
+        static int cont = 1;
+        static string simbolo = "X", posicao; 
+
+        static void Main(string[] args)
+        {
+            for (int L = 0; L < 3; L++) // Colocando posições de 0 a 8 no jogo; 
+            {
+                for (int C = 0; C < 3; C++)
+                {
+                    mat[L, C] = cont.ToString();
+                    cont++; 
+                }
+            }
+            MostrarVelha();
+            Console.Write($"Vai jogar {simbolo} em qual posição? ");
+            posicao = Console.ReadLine();
+            Jogar(simbolo, posicao);
+            MostrarVelha();
+        }
+        static void MostrarVelha()
+        {
+            Console.WriteLine("+---+---+---+");
+            for (int L = 0; L < 3; L++)
+            {
+                for (int C = 0; C < 3; C++)
+                {
+                    Console.Write("|  " + mat[L, C]);
+                }
+                Console.Write("|");
+                Console.WriteLine();
+                Console.WriteLine("+---+---+---+");
+            }
+        }
+        static bool Jogar(string simbolo, string posicao)
+        {
+            bool proximo = false;
+            for (int L = 0; L < 3; L++)
+            {
+                for (int C = 0; C < 3; C++)
+                {
+                    if (mat[L, C] == posicao)
+                    {
+                        mat[L, C] = simbolo;
+                        proximo = true; 
+                    }
+                }
+            }
+            return proximo;
+        }
+    }
+}
